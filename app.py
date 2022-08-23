fbffa23c06d4bd29bf9c8259e789f66d5d277159
leecodya <lee.codya@gmail.com>
2022-08-23 15:42:54 -0400

    getting the links to be flask friendly

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ my_name = "Cody Lee"
 def home():
     return render_template("home.htm", name_title = my_name)
 
+@app.route("/about")
+def about():
+    return render_template("about.htm", name_title = my_name)
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.htm", name_title = my_name)
+
+@app.route("/services")
+def services():
+    return render_template("services.htm", name_title = my_name)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.htm", name_title = my_name)
+
 if __name__ == "__main__":
     app.run()
